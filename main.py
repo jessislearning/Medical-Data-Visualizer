64379646b10baaf6c7174175a1e3a2a40dde6ad0
jessislearning a <jessica.afalla.29@gmail.com>
2024-03-25 05:21:00 +0000

    added overweight column, normalized cholesterol and glucose

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 # This entrypoint file to be used in development. Start by reading README.md
-import medical_data_visualizer
-from unittest import main
+#import medical_data_visualizer
+#from unittest import main
 
 # Test your function by calling it here
-medical_data_visualizer.draw_cat_plot()
-medical_data_visualizer.draw_heat_map()
+#medical_data_visualizer.draw_cat_plot()
+#medical_data_visualizer.draw_heat_map()
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+#main(module='test_module', exit=False)
+
+#----my code----
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+# Import data
+df = pd.read_csv("medical_examination.csv")
+
+#Calculate BMI
+BMI = df["weight"]/((df["height"])/100)**2
+
+# Add 'overweight' column
+df["overweight"] = (BMI > 25).astype(int)
+
+# normalize cholesterol and gluc
+df["cholesterol"] = (df["cholesterol"] > 1).astype(int)
+df["gluc"] = (df["gluc"] > 1).astype(int)
+
+print (df.head())
